30556f471a73073f5a26910a95c21ea26dd6bb9b
Amphakarn <pisuthigomol@yahoo.com>
2020-12-08 22:20:17 +0000

    add delete URL and update urls_index.ejs

--- express_server.js
+++ express_server.js
@@ -4,29 +4,57 @@ const PORT = 3002;
 // Set ejs as the view engine
 app.set("view engine", "ejs");
 
-let urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
-};
-
 // The body-parser library will convert the request body from a Buffer into string that we can read.
 // It will then add the data to the req(request) object under the key body.
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
+// create the URL database object
+let urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
 // generate a random string to serve as our shortURL
-function generateRandomString() {
-  let result = "";
+const generateNewKey = () => {
+  let newKey = "";
   let chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
   let charsLength = chars.length;
   for (let i = 0; i < 6; i++) {
-    result += chars.charAt(Math.floor(Math.random() * charsLength));
+    newKey += chars.charAt(Math.floor(Math.random() * charsLength));
   }
-  return result;
-}
+  return newKey; // does it require return?
+};
+
+// create actions
+
+// const showAllURLS = (urls) => {
+//   return urls;
+// };
+
+// const showOneURL = (urls, key) => {
+//   if(urls[key]) {
+//     return urls[key];
+//   } else {
+//     return null;
+//   }
+// };
+
+// const addOneURL = (urls, newURL) => {
+//   urls[newKey()] = newURL;
+// }
+
+// const removeURL = (urls, key) => {
+//   delete url[key];
+// };
+
+// const editURL = (urls, key, newURL) => {
+//   urls[key] = newURL;
+// };
 
 // GET method route
 // use res.render to load up an ejs view file
+
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
@@ -43,17 +71,30 @@ app.get("/urls/:shortURL", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {  
-  const shortRandomString = generateRandomString();
+  const shortURL = generateNewKey();
   const longURL = req.body.longURL;
   // add generated short URL and long URL to database
-  urlDatabase[shortRandomString] = longURL;
-  res.redirect(`/urls/${shortRandomString}`);
+  urlDatabase[shortURL] = longURL;
+  res.redirect(`/urls/${shortURL}`);
+  // console.log(urlDatabase);
 });
 
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL]
-  res.redirect(`${longURL}`);
+  console.log("This is the short URL: ", req.params.shortURL);
+  res.redirect(longURL);
 });
+
+// // Delete URL
+app.post("/urls/:shortURL/delete", (req, res) => {
+  const shortURL = req.params.shortURL;
+  // console.log("the database: ", urlDatabase);
+  // console.log("the short URL (key): ", shortURL);
+  // console.log("the long URL (value): ", urlDatabase[shortURL]);
+  delete urlDatabase[shortURL];
+  res.redirect("/urls");
+
+})
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
